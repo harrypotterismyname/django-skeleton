238e143905a389b5f98395a0826c079825e06276
hpthang <hpthang88@gmail.com>
2013-06-29 13:12:17 +0700

    update

--- mylist/models.py
+++ mylist/models.py
@@ -3,3 +3,16 @@ from django.utils.translation import ugettext_lazy as _
 from userena.utils import user_model_label
 
 
+class CheckList(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=1024, blank=True)
+    slug = models.CharField(max_length=512, blank=True)
+    created_at = models.DateTimeField(null=True)
+    is_deleted = models.BooleanField(default=False)
+
+class Task(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=1024, blank=True)
+    is_checked = models.BooleanField(default=False)
+    is_deleted = models.BooleanField(default=False)
+    id_check_list = models.ForeignKey(CheckList, null=True)
