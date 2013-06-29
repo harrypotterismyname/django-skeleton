01c98060ce4bcb1dddf3ffcd7c90a50b5a2244c1
anhtran <anhtran@moon9.org>
2013-06-29 16:25:47 +0700

    public page tpl

--- mylist/views.py
+++ mylist/views.py
@@ -3,3 +3,7 @@ from django.views.generic import TemplateView
 
 class HomeIndex(TemplateView):
     template_name = "mylist/home.html"
+
+
+class PublicView(TemplateView):
+    template_name = "mylist/public.html"
